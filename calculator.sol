0cf1251dfa89b4590b89d4e772780bfd648af651
Zakarialk <53450764+Zakarialk01@users.noreply.github.com>
2023-12-18 15:05:00 +0100

    Create calculator.sol

---
+++ calculator.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+contract calculator {
+
+    uint256 result = 0;
+
+    function add(uint256 num) public{
+        result += num;
+    } 
+    function sub(uint256 num) public{
+        result -= num;
+    }   
+    function multiply(uint256 num) public{
+        result *= num;
+    } 
+    function get() public view returns(uint256){
+        return  result;
+    }
+}
+
